19143585a7aec935d660c713ab24151cc48cf562
Chris Christodoulidis <cyberdotnet@gmail.com>
2020-11-12 00:32:40 +0000

    assets and notification test

--- RedisConsoleDesktop/Controllers/InstanceController.cs
+++ RedisConsoleDesktop/Controllers/InstanceController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +29,32 @@ namespace RedisConsoleDesktop.Controllers
             List<InstanceSettingsViewModel> m = new List<InstanceSettingsViewModel>();
             m.Add(new InstanceSettingsViewModel());
             var model = new InstanceSettingsViewModel();
+
+            if (HybridSupport.IsElectronActive)
+            {
+                Electron.IpcMain.On("basic-noti", (args) => {
+
+                    var options = new NotificationOptions("Basic Notification", "Short message part")
+                    {
+                        OnClick = async () => await Electron.Dialog.ShowMessageBoxAsync("Notification clicked")
+                    };
+
+                    Electron.Notification.Show(options);
+
+                });
+
+                Electron.IpcMain.On("advanced-noti", (args) => {
+
+                    var options = new NotificationOptions("Notification with image", "Short message plus a custom image")
+                    {
+                        OnClick = async () => await Electron.Dialog.ShowMessageBoxAsync("Notification clicked"),
+                        Icon = "/imgs/android-chrome-512x512.png"
+                    };
+
+                    Electron.Notification.Show(options);
+                });
+            }
+
             return View(model);
         }
 
