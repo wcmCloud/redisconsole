e661a305a2846aa4476f9d2e0768639a34b409ef
Chris Christodoulidis <cyberdotnet@gmail.com>
2020-11-21 22:23:07 +0000

    Instance details info client template

---
+++ RedisConsoleDesktop/Core/KendoBadgeHelpers.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisConsoleDesktop.Core
+{
+    public static class KendoBadgeHelpers
+    {
+
+        public static string LargeVariableRoundedBadge
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-lg k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgePrimary
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-primary k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeSecondary
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-secondary k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeTertiary
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-tertiary k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeInfo
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-info k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeSuccess
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-success k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeWarning
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-warning k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeError
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-error k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeDark
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-dark k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeLight
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-light k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+        public static string MediumRoundedBadgeInverse
+        {
+            get
+            {
+                return " k-badge k-badge-solid k-badge-inverse k-badge-md k-badge-rounded k-badge-border-cutout k-badge-inline ";
+            }
+        }
+
+    }
+}
+
--- RedisConsoleDesktop/Models/InstanceInfoGridViewModel.cs
+++ RedisConsoleDesktop/Models/InstanceInfoGridViewModel.cs
@@ -1,30 +1,40 @@
-﻿using System;
+﻿using RedisConsoleDesktop.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RedisConsoleDesktop.Models
 {
-    
+
     public class InstanceInfoGridViewModel
     {
 
         public string Key { get; set; }
         public string Value { get; set; }
+        public string VClientTemplate { get; set; }
+
 
 
         public InstanceInfoGridViewModel(KeyValuePair<string, string> item)
         {
-            if ((item.Value == null) || (item.Value == ""))
+            if (item.Key.StartsWith(" - ") && ((item.Value == null) || (item.Value == "")))
             {
                 Key = "";
-                Value = item.Key;
-
+                Value = item.Key?.Replace(" - ", "");
+                VClientTemplate = "";
+                if (Value.ToLower().Contains("true"))
+                    VClientTemplate = "<span class='" + KendoBadgeHelpers.MediumRoundedBadgePrimary + "k-badge-info" + "'>" + Value + "</span>";
+                else if (Value.ToLower().Contains("false"))
+                    VClientTemplate = "<span class='" + KendoBadgeHelpers.MediumRoundedBadgeWarning + "k-badge-error" + "'>" + Value + "</span>";
+                else
+                    VClientTemplate = "<span>" + Value + "</span>";
             }
             else
             {
                 Key = item.Key;
                 Value = item.Value;
+                VClientTemplate = "<span>" + Value + "</span>";
             }
         }
 
