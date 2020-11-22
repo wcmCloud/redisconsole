64fea667fa56fa717e40330fb6e4238691be20a3
Chris Christodoulidis <cyberdotnet@gmail.com>
2020-11-22 01:57:41 +0000

    Export to excel detail grid with templated column

---
+++ RedisConsoleDesktop/Controllers/ExportController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisConsoleDesktop.Controllers
+{
+    public class ExportController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult Excel_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+    }
+}
